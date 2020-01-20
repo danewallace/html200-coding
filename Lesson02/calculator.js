768e32615354457093bd6789dffb46fd9c2cdbf6
danewallace <danewallace@icloud.com>
2020-01-19 16:36:43 -0800

    Added variables and console output for lesson 2 assignment

---
+++ Lesson02/calculator.js
@@ -0,0 +1,11 @@
+const x = 10;
+const y = 11;
+const z = 100;
+const stringTest = '101';
+
+console.log(x + y);
+console.log(x * z);
+console.log(x + ' ' + stringTest);
+console.log('x' + ' ' + stringTest);
+console.log(z % x);
+console.log(z % y);
