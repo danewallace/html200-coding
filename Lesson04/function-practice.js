7cad24cf225a836bee38e7c5d2c940e55a9e8fa2
danewallace <danewallace@icloud.com>
2020-02-28 13:37:19 -0800

    Added lesson 4 folder

---
+++ Lesson04/function-practice.js
@@ -0,0 +1,11 @@
+const x = 25;
+const y = 3;
+let result = 0;
+
+function multiplier(firstNumber, secondNumber) {
+  result = firstNumber * secondNumber;
+}
+
+multiplier(x, y);
+
+console.log(result);
