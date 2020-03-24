dd3f049b9a7ac3e9e2db2a791533895719617f39
Dane Wallace <danewallace@icloud.com>
2020-03-24 12:54:01 -0700

    Continuing Lesson05 assignment

--- Lesson05/Assignment/main.js
+++ Lesson05/Assignment/main.js
@@ -1,69 +1,50 @@
 let balance = 500;
 
-  function optionsPrompt() {
-    let input = prompt(
+function optionsPrompt() {
+  let input = prompt(
       'Please make a selection:' + '\n' +
       'W to withdraw' + '\n' +
       'D to deposit' + '\n' +
       'B to view balance' + '\n' +
       'Q to quit'
-    );
+  );
+  selectOption(input);
+}
 
-    if (input === 'W' || 'w') {
+function withdrawMoney() {
+  let withdrawAmount = prompt('Please enter amount to withdraw:');
+  balance -= withdrawAmount;
+  alert('You have withdrawn $' + withdrawAmount);
+  optionsPrompt();
+}
+
+function depositMoney() {
+  let depositAmount = prompt('Please enter amount to deposit:');
+  balance += depositAmount;
+  alert('You have deposited $' + depositAmount);
+  optionsPrompt();
+}
+
+function viewBalance() {
+  alert('Your balance is $' + balance);
+  optionsPrompt();
+}
+
+function selectOption(input) {
+  switch(input) {
+    case 'W' || 'w': {
       withdrawMoney();
-    } else if (input === 'D' || 'd') {
+    }
+    case 'D' || 'd': {
       depositMoney();
-    } else if (input === 'B' || 'b') {
+    }
+    case 'B' || 'b': {
       viewBalance();
-    } else {
-      alert('Please enter an appropriate key');
-      optionsPrompt();
+    }
+    case 'Q' || 'q': {
+      break;
     }
   }
-
-  function withdrawMoney() {
-    let withdrawAmount = prompt('Please enter amount to withdraw:');
-    balance -= withdrawAmount;
-    alert('You have withdrawn $' + withdrawAmount);
-    optionsPrompt();
-  }
-
-  function depositMoney() {
-    let depositAmount = prompt('Please enter amount to deposit:');
-    balance += depositAmount;
-    alert('You have deposited $' + depositAmount);
-    optionsPrompt();
-  }
-
-  function viewBalance() {
-    alert('Your balance is $' + balance);
-    optionsPrompt();
-  }
-
+}
 
 optionsPrompt();
-
-
-
-// function banking(input) {
-//   switch(input) {
-//     case 'W' || 'w': {
-//       let withdrawAmount = prompt('Please enter amount to withdraw:');
-//       balance = balance - withdrawAmount;
-//       return withdrawAmount;
-//
-//     }
-//     case 'D' || 'd': {
-//       let depositAmount = prompt('Please enter amount to deposit:');
-//       balance = balance + depositAmount;
-//       return depositAmount;
-//       break;
-//     }
-//     case 'B' || 'b': {
-//       alert(`Your balance is: ${balance}`);
-//       break;
-//     }
-//     case 'Q' || 'q': {
-//       break;
-//     }
-//   }
