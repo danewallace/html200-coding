188ec083446eea1d624eb18069ff1929d42d591e
danewallace <danewallace@icloud.com>
2020-03-25 14:09:15 -0700

    Added lesson05 assignment with extra challenges

--- Lesson05/Assignment/main.js
+++ Lesson05/Assignment/main.js
@@ -1,28 +1,49 @@
 let balance = 500;
 
+optionsPrompt();
+
 function optionsPrompt() {
   let input = prompt(
-      'Please make a selection:' + '\n' +
-      'W to withdraw' + '\n' +
-      'D to deposit' + '\n' +
-      'B to view balance' + '\n' +
-      'Q to quit'
+    'Please make a selection:' + '\n' +
+    'W to withdraw' + '\n' +
+    'D to deposit' + '\n' +
+    'B to view balance' + '\n' +
+    'Q to quit'
   );
   selectOption(input);
 }
 
 function withdrawMoney() {
   let withdrawAmount = prompt('Please enter amount to withdraw:');
-  balance -= withdrawAmount;
-  alert('You have withdrawn $' + withdrawAmount);
-  optionsPrompt();
+  if (balance - Number(withdrawAmount) < 0) {
+    alert('Sorry, you have insufficient funds. Please try again.');
+    optionsPrompt();
+  } else if (balance - Number(withdrawAmount) < 300) {
+    let confirm = prompt('Your balance will be under $300. Please press c to confirm, press any other key to try again');
+    if (confirm == 'c' || confirm == 'C') {
+      balance -= Number(withdrawAmount);
+      alert('You have withdrawn $' + withdrawAmount);
+      optionsPrompt();
+    } else {
+      optionsPrompt();
+    }
+  } else {
+    balance -= Number(withdrawAmount);
+    alert('You have withdrawn $' + withdrawAmount);
+    optionsPrompt();
+  }
 }
 
 function depositMoney() {
   let depositAmount = prompt('Please enter amount to deposit:');
-  balance += depositAmount;
-  alert('You have deposited $' + depositAmount);
-  optionsPrompt();
+  if (Number(depositAmount) > 50000) {
+    alert('Sorry, the deposit limit for this account is $50,000. Please try again.');
+    optionsPrompt();
+  } else {
+    balance += Number(depositAmount);
+    alert('You have deposited $' + depositAmount);
+    optionsPrompt();
+  }
 }
 
 function viewBalance() {
@@ -32,19 +53,21 @@ function viewBalance() {
 
 function selectOption(option) {
   switch(option) {
-    case 'W' || 'w': {
+    case 'W', 'w': {
       withdrawMoney();
+      break;
     }
-    case 'D' || 'd': {
+    case 'D', 'd': {
       depositMoney();
+      break;
     }
-    case 'B' || 'b': {
+    case 'B', 'b': {
       viewBalance();
+      break;
     }
-    case 'Q' || 'q': {
+    case 'Q', 'q': {
+      alert('You have exited. Thank you.')
       break;
     }
   }
 }
-
-optionsPrompt();
